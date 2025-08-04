b96df968bf66b1df5cd7b0bedb3684c8cb7eb78f
MOGR <mogr@mail.ru>
2025-08-04 18:36:58 +0300

    !add attestation class Product

--- src/main/java/org/epoui/attestation01/Person.java
+++ src/main/java/org/epoui/attestation01/Person.java
@@ -1,12 +1,18 @@
 package org.epoui.attestation01;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private double money;
+    private Product[] bag;
+    private int bagSize;
 
     public Person(String name, double money) {
         setName(name);
         setMoney(money);
+        this.bag = new Product[10]; // Начальный размер пакета с продуктами
+        this.bagSize = 0;
     }
 
     public String getName() {
@@ -32,5 +38,54 @@ public class Person {
             throw new IllegalArgumentException("Деньги не могут быть отрицательными");
         }
         this.money = money;
+    }
+
+    public Product[] getBag() {
+        Product[] result = new Product[bagSize];
+        System.arraycopy(bag, 0, result, 0, bagSize);
+        return result;
+    }
+
+    public boolean buyProduct(Product product) {
+        if (product.getCost() > money) {
+            return false;
+        }
+        money -= product.getCost();
+        if (bagSize >= bag.length) {
+            Product[] newBag = new Product[bag.length * 2];
+            System.arraycopy(bag, 0, newBag, 0, bag.length);
+            bag = newBag;
+        }
+        bag[bagSize++] = product;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        if (bagSize == 0) {
+            return name + " - Ничего не куплено";
+        }
+        StringBuilder sb = new StringBuilder(name + " - ");
+        for (int i = 0; i < bagSize; i++) {
+            sb.append(bag[i].getName());
+            if (i < bagSize - 1) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Double.compare(person.money, money) == 0 &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, money);
     }
 }
